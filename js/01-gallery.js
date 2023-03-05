27b680766719b841d9833a88aa53577a565a9f12
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-03-05 14:08:23 +0200

    optimized task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const gallery = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
@@ -11,7 +10,7 @@ function createGalleryMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -30,17 +29,17 @@ function onGalleryClick(event) {
 
   if (event.target.nodeName !== "IMG") return;
 
-  const instance = basicLightbox.create(
-    `
+  const instance = basicLightbox
+    .create(
+      `
     <img src="${event.target.dataset.source}" width="800" height="600">
 `,
-    {
-      onShow: () => window.addEventListener("keydown", onEscClose),
-      onClose: () => window.removeEventListener("keydown", onEscClose),
-    }
-  );
-
-  instance.show();
+      {
+        onShow: () => window.addEventListener("keydown", onEscClose),
+        onClose: () => window.removeEventListener("keydown", onEscClose),
+      }
+    )
+    .show();
 
   function onEscClose(e) {
     if (e.code === "Escape") {
